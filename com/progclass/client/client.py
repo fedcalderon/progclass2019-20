d47df0a8c6e22070c2cd7aad26c9717c8b5e252d
kinonumba9 <kinolavish9@icloud.com>
2020-03-23 19:59:19 -0400

    testing another commit

--- com/progclass/client/client.py
+++ com/progclass/client/client.py
@@ -8,30 +8,64 @@
 import zmq
 # Adding Python's logging library
 import logging
+#import zmq
+#import logging
 
-print("Hello Client")
 
-# Configuring logging example
+# create logger
+module_logger = logging.getLogger('spam_application.helper')
+
+class Pants:
+    def __init__(self):
+        self.logger = logging.getLogger('spam_application.helper.Helper')
+        self.logger.info('creating an instance of Helper')
+
+    def do_something(self):
+        self.logger.info('doing something')
+        a = 1 + 1
+        self.logger.info('done doing something')
+
+def some_function():
+    module_logger.info('received a call to "some_function"')
+
+
+
+
+
+"""logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(levelname)s - CLIENT - %(message)s')
+file_handler = logging.FileHandler('client.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.info('waiting for next request from client')
+
+logger.debug('This will get logged to a file')
+logger.info('This will get logged to a file')
+logging.warning('This will get logged to a file')
+logger.error('This will get logged to a file')
+logger.critical('This will get logged to a file')"""
+
 logging.basicConfig(filename='client.log', filemode='w', format='%(asctime)s - %(levelname)s - CLIENT - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-# In order to start ZeroMQ, create a context
+logging.basicConfig(level= logging.DEBUG)
+
+
+filename = 'client.log'
+with open(filename) as file_object:
+    for line in file_object:
+        logging.debug(line)
+
 context = zmq.Context()
-# Create a zmq socket to talk to the server
+#  Socket to talk to server
+print("Connecting to hello world server…")
 socket = context.socket(zmq.REQ)
-# Connect zmq socket to localhost
 socket.connect("tcp://localhost:5555")
-#  Show message on console
-print("Connecting to hello server…")
-# Show message on the log file
-logging.warning("Connecting to hello server…")
-# Create a string variable to store user inputs and initialize to empty
 user_input = ""
-# Start an infinite loop
 while user_input != "q":
-    user_input = input("Type a msg ('q' to quit program): ")
+    user_input = input("Type a num: ")
     if user_input != "q":
-        # The socket can send literal strings to the server
         socket.send_string(user_input)
         logging.warning(f"Message to server: {user_input}")
         #  Get the reply.
         message = socket.recv()
-        logging.warning(f"Reply from server: {message}")
+        logging.debug(message)
--- com/progclass/server/server.py
+++ com/progclass/server/server.py
@@ -6,27 +6,48 @@
 
 import time
 import zmq
+
 import logging
+import pants
 
-print("This is kino... testing commit")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(levelname)s - SERVER - %(message)s')
+file_handler = logging.FileHandler('server.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.info('waiting for next request from server')
 
-# Initialize logging example
-logging.basicConfig(filename='server.log', filemode='w', format='%(asctime)s - %(levelname)s - SERVER - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logger.debug('This will get logged to a file')
+logger.info('This will get logged to a file')
 logging.warning('This will get logged to a file')
+logger.error('This will get logged to a file')
+logger.critical('This will get logged to a file')
 
-# Create a ZeroMQ context in order to talk to the client
+logging.basicConfig(filename='client.log', filemode='w', format='%(asctime)s - %(levelname)s - SERVER - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(level= logging.DEBUG)
+
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.info('Admin logged in')
 context = zmq.Context()
-# Create a zmq socket
 socket = context.socket(zmq.REP)
-# Bind zmq socket to listen from any IP address on port 5555
-socket.bind("tcp://*:5555")
-# Start infinite loop
+socket.bind("tcp://*:5555") #localhost xx.xx.xx.xx:port
+
+filename = 'server.log'
+with open(filename) as file_object:
+    for line in file_object:
+        logging.debug(line)
+
+counter = 0
+num1 = num2 = 0
+
 while True:
     #  Wait for next request from client
     message = socket.recv_string()
-    #  Do some 'work' like wait for 1 second
+
+    #  Do some 'work'
     time.sleep(1)
-    # Show message on log
     logging.warning(f"Message from client: {message}")
     #  Send reply back to client
     socket.send_string(message)
